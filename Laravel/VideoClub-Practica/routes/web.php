c929a14fb92605d05f928b91759a0eda562bc7cd
Rares <raresgmm@educastur.es>
2025-02-12 14:17:06 +0100

    Views

--- Laravel/VideoClub-Practica/routes/web.php
+++ Laravel/VideoClub-Practica/routes/web.php
@@ -14,31 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ('Pantalla principal');
+    return view('home');
 });
 
 Route::get('login', function () {
-    return ('Login usuario');
-});
-
-Route::get('logout', function () {
-    return ('Logout usuario');
+    return view('auth.login');
 });
 
 Route::get('catalog', function () {
-    return ('Listado peliculas');
+    return view('catalog.index');
 });
 
 Route::get('catalog/show/{id}', function ($id) {
-    return 'Vista detalle película '.$id;
+    return view('catalog.show', array('id'=>$id));
 });
 
 Route::get('catalog/create', function () {
-    return ('Añadir película');
+    return view('catalog.create');
 });
 
 Route::get('catalog/edit/{id}', function ($id) {
-    return 'welcome '.$id;
+    return view('catalog.edit');
 });
 
 Route::get('home', function () {
