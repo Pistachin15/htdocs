7d7e62c406dc5a67173857039200eaa64d7737f3
Rares <raresgmm@educastur.es>
2025-03-06 14:19:03 +0100

    Avance con bases de datos

---
+++ Laravel/MIVIDEOCLUB_Bueno/database/migrations/2025_03_06_114106_create_personas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('apellidos', 100);
+            $table->integer('edad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ Laravel/MIVIDEOCLUB_Bueno/database/migrations/2025_03_06_124319_add_telefono_to_personas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->string('telefono', 20); // O ajusta el tipo y tamaño según sea necesario
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->dropColumn('telefono');
+        });
+    }
+};
